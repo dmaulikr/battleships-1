7a8086d6c8dbaab917b4272c7646289456a4504e
Karim Kawambwa <karim.kawambwa@shopify.com>
2014-10-31 16:06:28 -0400

    Created project with SpriteKit just incase and added Facebook pop using pods

---
+++ Battleship/Battleship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Battleship
+//
+//  Created by Karim Kawambwa on 2014-10-31.
+//  Copyright (c) 2014 Martijn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Battleship/Battleship/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Battleship
+//
+
+//  Copyright (c) 2014 Martijn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
